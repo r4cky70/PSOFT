d8c23fee261e9cce48e60cc37d557d8a1b593b43
Rodrigo Estrela <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2023-03-28 16:50:13 -0300

    Pratica 2 primeiro commit

---
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/repository/LoteRepository.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+public interface LoteRepository<T, ID> {
+
+    T save(T lote);
+    T find(ID id);
+
+    List<T> findAll();
+    T update(T lote);
+    void delete(T lote);
+    void deleteAll();
+}
+
---
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepository.java
@@ -0,0 +1,43 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class VolatilLoteRepository implements LoteRepository<Lote, Long> {
+    List<Lote> lotes = new ArrayList<>();
+
+    @Override
+    public Lote save(Lote lote) {
+        lotes.add(lote);
+        return lotes.stream().findFirst().get();
+    }
+
+    @Override
+    public Lote find(Long id) {
+        return lotes.get(Integer.parseInt("" + id));
+    }
+
+    @Override
+    public List<Lote> findAll() {
+        return lotes;
+    }
+
+    @Override
+    public Lote update(Lote lote){
+        lotes.clear();
+        lotes.add(lote);
+        lotes.stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public void delete(Lote lote){
+        lotes.clear();
+    }
+
+    @Override
+    public void deleteAll(){
+        lotes.clear();
+    }
+}
